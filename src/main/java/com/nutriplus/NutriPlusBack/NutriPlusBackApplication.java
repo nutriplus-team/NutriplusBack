844cec9e9adfd2088bf5eac4f0794d3b23c7cb41
Lucas <lucas.tnagel@gmail.com>
2020-03-09 15:40:52 -0400

    Adding files to repository

---
+++ src/main/java/com/nutriplus/NutriPlusBack/NutriPlusBackApplication.java
@@ -0,0 +1,13 @@
+package com.nutriplus.NutriPlusBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriPlusBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriPlusBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutriplus/NutriPlusBack/NutriPlusBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutriplus.NutriPlusBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriPlusBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
