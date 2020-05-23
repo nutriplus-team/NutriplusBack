154c344f80f7d45036bf402cac545d72807ce4f7
omsf <ocimar.acad@gmail.com>
2020-05-23 14:16:41 -0300

    application.propieties eddited

--- src/main/java/com/nutriplus/NutriPlusBack/Repositories/ApplicationUserRepository.java
+++ src/main/java/com/nutriplus/NutriPlusBack/Repositories/ApplicationUserRepository.java
@@ -4,7 +4,9 @@ import com.nutriplus.NutriPlusBack.Domain.UserCredentials;
 import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ApplicationUserRepository extends Neo4jRepository<UserCredentials, Long> {
     UserCredentials findByUsername(String username);
     UserCredentials findByEmail(String email);
