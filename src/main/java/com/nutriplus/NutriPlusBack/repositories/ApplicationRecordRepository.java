5b1ca5a51cdfc900d1eeb5de431d462dee4ef940
Gianluigi Dal Toso <gianluigi.toso@gmail.com>
2020-08-12 13:59:16 -0300

    Add query from repository

--- src/main/java/com/nutriplus/NutriPlusBack/repositories/ApplicationRecordRepository.java
+++ src/main/java/com/nutriplus/NutriPlusBack/repositories/ApplicationRecordRepository.java
@@ -1,7 +1,12 @@
 package com.nutriplus.NutriPlusBack.repositories;
 
 import com.nutriplus.NutriPlusBack.domain.patient.PatientRecord;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 
+import java.util.ArrayList;
+
 public interface ApplicationRecordRepository extends Neo4jRepository<PatientRecord, Long> {
+    @Query("MATCH (p:Patient)-[:HAS_RECORD]->(r:PatientRecord) WHERE p.uuid=$0 RETURN r.uuid")
+    ArrayList<String> getUuidFromPatientRecords(String uuid);
 }
