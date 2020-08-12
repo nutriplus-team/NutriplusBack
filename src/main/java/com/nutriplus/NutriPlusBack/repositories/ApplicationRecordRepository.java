a7537fd36eea214aa2a5519ebf793cec5e99bab2
Gianluigi Dal Toso <gianluigi.toso@gmail.com>
2020-08-12 14:43:47 -0300

    fix mistype

--- src/main/java/com/nutriplus/NutriPlusBack/repositories/ApplicationRecordRepository.java
+++ src/main/java/com/nutriplus/NutriPlusBack/repositories/ApplicationRecordRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.neo4j.repository.Neo4jRepository;
 import java.util.ArrayList;
 
 public interface ApplicationRecordRepository extends Neo4jRepository<PatientRecord, Long> {
-    @Query("MATCH (r:PatientRecord)-[:HAS_MENU]->(m:Menu) WHERE r.uuid=$0 RETURN m.uuid")
+    @Query("MATCH (r:PatientRecord)-[:HAS_MENUS]->(m:Menu) WHERE r.uuid=$0 RETURN m.uuid")
     ArrayList<String> getMenusUuidsFromPatientRecord(String uuid);
 }
